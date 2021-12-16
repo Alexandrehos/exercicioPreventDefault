d2da65ba9a0c2bc1c2ee825b181e6ca24c2527e1
Alexandre Filho <xandehos@gmail.com>
2021-12-16 00:18:03 -0300

    Projeto pronto

---
+++ script.js
@@ -0,0 +1,201 @@
+const nomePokemons = [
+  "Bulbasaur",
+  "Ivysaur",
+  "Venusaur",
+  "Charmander",
+  "Charmeleon",
+  "Charizard",
+  "Squirtle",
+  "Wartortle",
+  "Blastoise",
+  "Caterpie",
+  "Metapod",
+  "Butterfree",
+  "Weedle",
+  "Kakuna",
+  "Beedrill",
+  "Pidgey",
+  "Pidgeotto",
+  "Pidgeot",
+  "Rattata",
+  "Raticate",
+  "Spearow",
+  "Fearow",
+  "Ekans",
+  "Arbok",
+  "Pikachu",
+  "Raichu",
+  "Sandshrew",
+  "Sandslash",
+  "Nidoran♀",
+  "Nidorina",
+  "Nidoqueen",
+  "Nidoran♂",
+  "Nidorino",
+  "Nidoking",
+  "Clefairy",
+  "Clefable",
+  "Vulpix",
+  "Ninetales",
+  "Jigglypuff",
+  "Wigglytuff",
+  "Zubat",
+  "Golbat",
+  "Oddish",
+  "Gloom",
+  "Vileplume",
+  "Paras",
+  "Parasect",
+  "Venonat",
+  "Venomoth",
+  "Diglett",
+  "Dugtrio",
+  "Meowth",
+  "Persian",
+  "Psyduck",
+  "Golduck",
+  "Mankey",
+  "Primeape",
+  "Growlithe",
+  "Arcanine",
+  "Poliwag",
+  "Poliwhirl",
+  "Poliwrath",
+  "Abra",
+  "Kadabra",
+  "Alakazam",
+  "Machop",
+  "Machoke",
+  "Machamp",
+  "Bellsprout",
+  "Weepinbell",
+  "Victreebel",
+  "Tentacool",
+  "Tentacruel",
+  "Geodude",
+  "Graveler",
+  "Golem",
+  "Ponyta",
+  "Rapidash",
+  "Slowpoke",
+  "Slowbro",
+  "Magnemite",
+  "Magneton",
+  "Farfetch'd",
+  "Doduo",
+  "Dodrio",
+  "Seel",
+  "Dewgong",
+  "Grimer",
+  "Muk",
+  "Shellder",
+  "Cloyster",
+  "Gastly",
+  "Haunter",
+  "Gengar",
+  "Onix",
+  "Drowzee",
+  "Hypno",
+  "Krabby",
+  "Kingler",
+  "Voltorb",
+  "Electrode",
+  "Exeggcute",
+  "Exeggutor",
+  "Cubone",
+  "Marowak",
+  "Hitmonlee",
+  "Hitmonchan",
+  "Lickitung",
+  "Koffing",
+  "Weezing",
+  "Rhyhorn",
+  "Rhydon",
+  "Chansey",
+  "Tangela",
+  "Kangaskhan",
+  "Horsea",
+  "Seadra",
+  "Goldeen",
+  "Seaking",
+  "Staryu",
+  "Starmie",
+  "Mr. Mime",
+  "Scyther",
+  "Jynx",
+  "Electabuzz",
+  "Magmar",
+  "Pinsir",
+  "Tauros",
+  "Magikarp",
+  "Gyarados",
+  "Lapras",
+  "Ditto",
+  "Eevee",
+  "Vaporeon",
+  "Jolteon",
+  "Flareon",
+  "Porygon",
+  "Omanyte",
+  "Omastar",
+  "Kabuto",
+  "Kabutops",
+  "Aerodactyl",
+  "Snorlax",
+  "Articuno",
+  "Zapdos",
+  "Moltres",
+  "Dratini",
+  "Dragonair",
+  "Dragonite",
+  "Mewtwo",
+  "Mew",
+];
+
+const baseURL =
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
+
+function criaTwet() {
+  const tweet = document.createElement("div");
+  tweet.setAttribute("class", "tweet");
+  const topoTwet = document.createElement("div");
+  topoTwet.setAttribute("class", "topoTwet");
+  const imagem = document.createElement("img");
+  imagem.setAttribute("class", "imagem");
+  const nome = document.createElement("span");
+  nome.setAttribute("class", "nome");
+  const arroba = document.createElement("p");
+  arroba.setAttribute("class", "arroba");
+  const conteudo = document.createElement("p");
+  conteudo.setAttribute("class", "conteudo");
+  topoTwet.append(imagem, nome);
+  tweet.append(topoTwet, arroba, conteudo);
+  return tweet;
+}
+
+function escolheNum() {
+  return Math.floor(Math.random() * 151);
+}
+
+const botao = document.querySelector("#formulario");
+botao.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const entrada = document.querySelector("#textoDoTweet");
+  document.querySelector("#tweets").append(teste(entrada.value));
+  entrada.value = "";
+});
+
+function teste(conteudo) {
+  const elemento = criaTwet();
+  elemento.querySelector(".conteudo").innerText = conteudo;
+  const pokemonNum = escolheNum();
+  const pokemonNome = nomePokemons[pokemonNum - 1];
+  elemento
+    .querySelector(".imagem")
+    .setAttribute("src", `${baseURL}${pokemonNum}.png`);
+  elemento.querySelector(".nome").innerText = pokemonNome;
+  elemento.querySelector(".arroba").innerText = `@${("00" + pokemonNum).slice(
+    -3
+  )}-${pokemonNome}`;
+  return elemento;
+}
